cc844cb6b9e0f6155427f4c7f3a7c2b3a051c470
Rizky Benggolo <travarilo@gmail.com>
2019-02-07 10:51:52 +0700

    onclite: Add prebuilt RetroMusic
    Signed-off-by: fadlyas07 <fadlyardhians@gmail.com>

--- device.mk
+++ device.mk
@@ -329,6 +329,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
+# RetroMusic
+PRODUCT_PACKAGES += \
+    RetroMusic
+
 # Seccomp policy
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
