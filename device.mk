484b6d9646e47483259472d8593faa89b67308bf
ragy <wad.moskla@gmail.com>
2020-04-16 12:22:26 +0200

    onc(lite): Include Google Camera Go Edition
    * At least better then Snap

--- device.mk
+++ device.mk
@@ -130,7 +130,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     libgui_vendor \
-    Snap
+    GCam
 
 # Display
 PRODUCT_PACKAGES += \
