d0c2c78268211cc8df2ac08dfb94b27057fcf257
vineelsai <vineelsai5439@gmail.com>
2020-07-04 20:59:34 +0530

    pixelify

--- aosp_onclite.mk
+++ aosp_onclite.mk
@@ -16,7 +16,7 @@ $(call inherit-product, device/xiaomi/onclite/device.mk)
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_onclite
+PRODUCT_NAME := aosp_onclite
 PRODUCT_DEVICE := onclite
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 7
