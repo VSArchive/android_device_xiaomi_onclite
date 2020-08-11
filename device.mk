05f48d1152fb61995186dcfb7e91658d8211de27
Vineel Sai <vineelsai5439@gmail.com>
2020-08-11 08:28:47 +0530

    added rro excludes

--- device.mk
+++ device.mk
@@ -17,13 +17,12 @@
 $(call inherit-product, vendor/xiaomi/onclite/onclite-vendor.mk)
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
-# PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-#     $(LOCAL_PATH)/overlay-lineage/lineage-sdk
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-lineage/lineage-sdk
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
