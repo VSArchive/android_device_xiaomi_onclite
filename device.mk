6b3c63f06594a13b816b7c2ff38547241e0ed0d8
Vineel Sai <vineelsai26@gmail.com>
2020-09-11 11:00:18 +0000

    nuke retromusic

--- device.mk
+++ device.mk
@@ -337,10 +337,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
-# RetroMusic
-PRODUCT_PACKAGES += \
-    RetroMusic
-
 # Seccomp policy
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
